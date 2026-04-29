28d08a8a41fe24bcf9c4cc33737a6ce51b2b04b6
Sweksha Dhungana <swekshadhungana@gmail.com>
2026-04-29 16:38:39 +0545

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+console.log(__dirname, "dirname");
+
+//make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
