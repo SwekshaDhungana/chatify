28faff806d106add60e82ccff5b6eb73a5a47644
Sweksha Dhungana <swekshadhungana@gmail.com>
2026-04-30 09:57:38 +0545

    minor changes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,4 +1,9 @@
 import express from "express";
+import { signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
+router.post("/signup", signup);
+// router.get("/signin", (req, res) => {
+//   res.send("test");
+// });
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import path from "path";
+import { connectDB } from "./db/config.js";
+import authRoutes from "./routes/auth.route.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const __dirname = path.resolve();
-console.log(__dirname, "dirname");
+// console.log(__dirname, "dirname");
 
 //make ready for deployment
 if (process.env.NODE_ENV === "production") {
@@ -15,6 +19,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+  });
 });
