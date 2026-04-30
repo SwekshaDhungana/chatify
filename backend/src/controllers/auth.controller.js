997ce5fec87bec3fc38f28f4bb6ebece41e38f16
Sweksha Dhungana <swekshadhungana@gmail.com>
2026-04-30 22:20:56 +0545

    added -password in auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -104,7 +104,7 @@ export const updateProfile = async (req, res) => {
       userId,
       { profilePic: uploadResponse.secure_url },
       { new: true },
-    );
+    ).select("-password");
     res.status(200).json(updatedUser);
   } catch (error) {
     console.error("Error in the update profile", error);
