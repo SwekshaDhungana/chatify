7ded42550c8528249e0bbd11779620d3d34e6fee
Sweksha Dhungana <swekshadhungana@gmail.com>
2026-05-02 13:36:21 +0545

    built signup page for frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./db/config.js";
 import authRoutes from "./routes/auth.route.js";
@@ -8,6 +9,7 @@ import { ENV } from "./lib/env.js";
 const app = express();
 const PORT = ENV.ENVPORT || 3000;
 app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
